a740d35a854018b0270af174e7c94815a7dd0447
HayamaUmi <umi@hayama.cf>
2016-01-28 13:17:27 +0800

    add a sample plugin base class

---
+++ PluginInterface/Plugins/SamplePluginBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using PluginLoader.Plugins;
+
+namespace PluginInterface.Plugins
+{
+	/// <summary>
+	/// Sample plugin base.
+	/// it is the sample base plugin
+	/// </summary>
+	public abstract class SamplePluginBase:IPlugin
+	{
+		protected SamplePluginBase ()
+		{
+		}
+
+		public virtual bool Loading()
+		{
+			return true;
+		}
+
+		public virtual bool UnLoading()
+		{
+			return true;
+		}
+		/// <summary>
+		/// Start the specified args.
+		/// </summary>
+		/// <param name="args">Arguments.</param>
+		public virtual void Start(object[] args)
+		{
+		}
+	}
+}
+
